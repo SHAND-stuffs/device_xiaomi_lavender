68875b2094ec59eadc3a829bb6d71f2a33e5d089
Bruno Martins <bgcngm@gmail.com>
2023-09-18 14:14:53 -0300

    lavender: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Ifcfb9330fe3a64c2f2de62b1d8a0a01eac052251

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Exclude TOF sensor from InputManager
 PRODUCT_COPY_FILES += \
