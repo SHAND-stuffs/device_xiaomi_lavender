a3d711f0dd621235863c43ce0678f3247ba968c0
SH4ND <miqueiasantos20@gmail.com>
2023-09-18 11:29:18 -0300

    lavender: Initial LineageOS bringup
    Change-Id: I6f0db2e690869ead376a11bf3f7c26d1ea4ea94d

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -9,15 +9,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common POSP stuff
-$(call inherit-product, vendor/potato/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from lavender device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := potato_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
