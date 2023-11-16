65322f72eb2cb1a84811b734fdf28289491f1562
SH4ND <miqueiasantos20@gmail.com>
2023-11-16 09:31:14 -0300

    lavender: Remove init
    * Idc if you have more or less RAM, I only have 4GB and I want to keep the tree cleaner
    
    Change-Id: I58d7d79d3a3604b59bbbc444db2618f38da2ba78

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,11 +97,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/framework_manifes
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_lavender
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
-TARGET_RECOVERY_DEVICE_MODULES := libinit_lavender
-
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
